38aa08f6a0abce425b6965bd191b4b55fcbedc23
Marwan Mai <emef4z@gmail.com>
2024-07-23 15:54:54 +0200

    Add Maven dependencies and Test Junit

---
+++ eventure-backend/src/test/java/com/mf4z/eventure/JUnitTestOne.java
@@ -0,0 +1,10 @@
+package com.mf4z.eventure;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitTestOne {
+    @Test
+    public void test(){
+        System.out.println("Assalamu'alaikum test!");
+    }
+}
